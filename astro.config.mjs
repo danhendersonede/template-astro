ad6e400f6ef67345f1942737625c8a367e7b6ada
Daniel Henderson-Ede <daniel@hendersonede.com>
2025-12-21 23:32:10 -0600

    feat: add icon library support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: import.meta.env.PUBLIC_SITE_URL || 'http://localhost:4321',
-  integrations: [sitemap(), mdx()],
+  integrations: [sitemap(), mdx(), icon()],
   prefetch: {
     defaultStrategy: 'hover',
   },
